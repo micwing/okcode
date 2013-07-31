303be46647ab6002a7c910c353e2b1df60c15f48
micwing <micwing@163.com>
2013-07-31 22:33:07 +0800

    baseConfig service

--- biz-trading-intf/src/main/java/okcode/biz/trading/dto/BaseConfigDto.java
+++ biz-trading-intf/src/main/java/okcode/biz/trading/dto/BaseConfigDto.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import okcode.biz.trading.util.ConfigConstants;
+
 
 public class BaseConfigDto {
 	
@@ -12,15 +14,85 @@ public class BaseConfigDto {
 	}
 	
 	public BaseConfigDto(Map<String, String> map) {
-		//TODO
+		this.systemName = map.get(ConfigConstants.BASECONFIG_SYSTEM_NAME);
+		this.siteLogoUrl = map.get(ConfigConstants.BASECONFIG_SITE_LOGO_URL);
+		this.siteUrl = map.get(ConfigConstants.BASECONFIG_SITE_URL);
+		this.siteKeyword = map.get(ConfigConstants.BASECONFIG_SITE_KEYWORD);
+		this.siteDescription = map.get(ConfigConstants.BASECONFIG_SITE_DESCRIPTION);
 	}
 	
 	public Map<String, String> toMap() {
 		Map<String, String> map = new HashMap<String, String>();
+		map.put(ConfigConstants.BASECONFIG_SYSTEM_NAME, getSystemName());
+		map.put(ConfigConstants.BASECONFIG_SITE_LOGO_URL, getSiteLogoUrl());
+		map.put(ConfigConstants.BASECONFIG_SITE_URL, getSiteUrl());
+		map.put(ConfigConstants.BASECONFIG_SITE_KEYWORD, getSiteKeyword());
+		map.put(ConfigConstants.BASECONFIG_SITE_DESCRIPTION, getSiteDescription());
 		return map;
 	}
 	
+	public static Set<String> getKeys() {
+		Set<String> set = new HashSet<String>();
+		set.add(ConfigConstants.BASECONFIG_SYSTEM_NAME);
+		set.add(ConfigConstants.BASECONFIG_SITE_LOGO_URL);
+		set.add(ConfigConstants.BASECONFIG_SITE_URL);
+		set.add(ConfigConstants.BASECONFIG_SITE_KEYWORD);
+		set.add(ConfigConstants.BASECONFIG_SITE_DESCRIPTION);
+		return set;
+	}
+	
 	private Long userId;
+	
+	private String systemName;
+	
+	private String siteLogoUrl;
+	
+	private String siteUrl;
+	
+	private String siteKeyword;
+	
+	private String siteDescription;
+	
+	
+	public String getSystemName() {
+		return systemName;
+	}
+
+	public void setSystemName(String systemName) {
+		this.systemName = systemName;
+	}
+
+	public String getSiteLogoUrl() {
+		return siteLogoUrl;
+	}
+
+	public void setSiteLogoUrl(String siteLogoUrl) {
+		this.siteLogoUrl = siteLogoUrl;
+	}
+
+	public String getSiteUrl() {
+		return siteUrl;
+	}
+
+	public void setSiteUrl(String siteUrl) {
+		this.siteUrl = siteUrl;
+	}
+
+	public String getSiteKeyword() {
+		return siteKeyword;
+	}
+
+	public void setSiteKeyword(String siteKeyword) {
+		this.siteKeyword = siteKeyword;
+	}
+
+	public String getSiteDescription() {
+		return siteDescription;
+	}
+
+	public void setSiteDescription(String siteDescription) {
+		this.siteDescription = siteDescription;
+	}
 
 	public Long getUserId() {
 		return userId;
@@ -28,12 +100,6 @@ public class BaseConfigDto {
 
 	public void setUserId(Long userId) {
 		this.userId = userId;
-	}
-	
-	public static Set<String> getKeys() {
-		Set<String> set = new HashSet<String>();
-//		set.add(e);
-		return set;
 	}
 	
 }
---
+++ biz-trading-intf/src/main/java/okcode/biz/trading/util/ConfigConstants.java
@@ -0,0 +1,46 @@
+package okcode.biz.trading.util;
+
+import okcode.service.standard.util.StandardConstants;
+
+
+public class ConfigConstants extends StandardConstants {
+	public static final String BASECONFIG_SYSTEM_NAME = "BASECONFIG_SYSTEM_NAME";
+	
+	public static final String BASECONFIG_SITE_LOGO_URL = "BASECONFIG_SITE_LOGO_URL";
+	
+	public static final String BASECONFIG_SITE_URL = "BASECONFIG_SITE_URL";
+	
+	public static final String BASECONFIG_SITE_KEYWORD = "BASECONFIG_SITE_KEYWORD";
+	
+	public static final String BASECONFIG_SITE_DESCRIPTION = "BASECONFIG_SITE_DESCRIPTION";
+	
+	public static final String EMAILCONFIG_SENDER_NAME = "EMAILCONFIG_SENDER_NAME";
+	
+	public static final String EMAILCONFIG_ADDRESS = "EMAILCONFIG_ADDRESS";
+	
+	public static final String EMAILCONFIG_STMP_SERVER = "EMAILCONFIG_STMP_SERVER";
+	
+	public static final String EMAILCONFIG_PASSWORD = "EMAILCONFIG_PASSWORD";
+	
+	public static final String RUNSTATUS_STATUS = "RUNSTATUS_STATUS";
+	
+	public static final String RUNSTATUS_RECOVER_TIME = "RUNSTATUS_RECOVER_TIME";
+	
+	public static final String RUNSTATUS_NOTICE = "RUNSTATUS_NOTICE";
+	
+	public static final String IMAGECONFIG_XXX = "";
+	
+	public static final String WATERMARKCONFIG_XXX = "";
+	
+	public static final String SAFETYCONFIG_ADMIN_VERIFICATION = "SAFETYCONFIG_ADMIN_VERIFICATION";
+	
+	public static final String SAFETYCONFIG_PORTAL_VERIFICATION = "SAFETYCONFIG_PORTAL_VERIFICATION";
+	
+	public static final String SAFETYCONFIG_CONSOLE_VERIFICATION = "SAFETYCONFIG_CONSOLE_VERIFICATION";
+	
+	public static final String SAFETYCONFIG_CONTENT_RECYCLE = "SAFETYCONFIG_CONTENT_RECYCLE";
+	
+	public static final String SAFETYCONFIG_XXX = "";
+	
+	public static final String FRONTHOMECONFIG_XXX = "";
+}
